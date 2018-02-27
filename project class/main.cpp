a2a3fee2c7555ee35644510fe2d05dca4895129b
Andres Saladrigas <36264953+TheArzhel@users.noreply.github.com>
2018-02-27 10:25:01 +0100

    new

--- project class/main.cpp
+++ project class/main.cpp
@@ -1,3 +1,8 @@
-int main() {
+
+#include "../SDL/include/SDL.h"
+#pragma comment(lib, "../SDL/libx86/SDl2.lib")
+#pragma comment(lib, "../SDL/libx86/SDl2main.lib")
+
+int main(int argc, char* argv[]) {
 	return 0;
 }
