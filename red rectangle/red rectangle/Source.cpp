8eed884e545fd111a79b223b4f4c3df430c564bf
Andres Saladrigas <36264953+TheArzhel@users.noreply.github.com>
2018-03-13 00:01:27 +0100

    add mixer folder
    add libriries, and include in code. works fine

--- red rectangle/red rectangle/Source.cpp
+++ red rectangle/red rectangle/Source.cpp
@@ -1,9 +1,11 @@
 #include "SDL/include/SDL.h"
 #include "SDL_Image/include/SDL_image.h"
+#include "SDL_Mixer/include/SDL_mixer.h"
 
 #pragma comment (lib,"SDL/libx86/SDL2.lib")
 #pragma comment (lib,"SDL/libx86/SDL2main.lib")
 #pragma comment (lib,"SDL_Image/libx86/SDL2_image.lib")
+#pragma comment (lib,"SDL_Mixer/libx86/SDL2_mixer.lib")
 
 SDL_Window* window; // to create a window, first make a ponter, then we specify
 SDL_Renderer* render; // pointer to render
